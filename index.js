c787410e01e92fa5ee55199c8ecd5a1f07071f9b
98smrav6 <89051027+98smrav6@users.noreply.github.com>
2022-03-07 15:57:01 -0500

    started index.js/assigned variables

---
+++ index.js
@@ -0,0 +1,32 @@
+const Manager  = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require(".lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+const teamMembers = [];
+const idArray = [];
+
+function appMenu() {
+    
+    function createManager() {
+        console.log("Please build your team");
+        inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the team manager's name?",
+            validate: answer => {
+              if (answer !== "") {
+                return true;
+              }
+              return "Please enter at least one character.";
+            }
+        },  
+}
